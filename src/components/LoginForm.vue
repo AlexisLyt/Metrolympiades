29ef8c62fe59dff9e1791bcc7fcdaa237cef6842
Alexis LOYANT <alexis.loyany@unicaen.fr>
2025-04-07 01:10:34 +0200

    First commit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2 class="text-center">Connexion</h2>
+  <form action="" class="flex flex-col gap-4">
+    <input class="p-2 rounded" type="email" name="UserEmail" id="user_email" placeholder="Email" />
+    <input class="p-2 rounded" type="password" name="UserPassword" id="user_password" placeholder="Mot de passe" />
+    <button class="bg-blue p-2 border rounded-lg" type="submit">Connexion</button>
+  </form>
+  <div class="flex gap-3 mt-2">
+    <p class="m-0">Pas encore de compte ?</p>
+    <a href="">Je m'inscris</a>
+  </div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginForm from '@/components/LoginForm.vue'
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen w-screen">
+    <div class="loginForm">
+      <LoginForm />
+    </div>
+  </div>
+</template>
