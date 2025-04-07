57ee2b19f52d3efc5394f82a290c37963c0d865a
Alexis LOYANT <alexis.loyany@unicaen.fr>
2025-04-07 20:18:33 +0200

    Add login system Fixed registration Fixed router

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,83 @@
 <script setup>
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const email = ref('')
+const password = ref('')
+const incorrectAuth = ref(false)
+
+const isFormValid = computed(() => {
+  return !!email.value.trim() && !!password.value.trim()
+})
+
+const isLoading = ref(false)
+
+function login() {
+  isLoading.value = true
+
+  fetch('http://localhost:3000/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Login successful')
+        return response.json()
+      } else {
+        console.log('Login failed')
+        incorrectAuth.value = true
+        return Promise.reject('Login failed')
+      }
+    })
+    .then((data) => {
+      localStorage.setItem('user', JSON.stringify(data))
+      router.push('/')
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+    .finally(() => {
+      isLoading.value = false
+    })
+}
 </script>
 
 <template>
   <h2 class="text-center">Connexion</h2>
-  <form action="" class="flex flex-col gap-4">
-    <input class="p-2 rounded" type="email" name="UserEmail" id="user_email" placeholder="Email" />
-    <input class="p-2 rounded" type="password" name="UserPassword" id="user_password" placeholder="Mot de passe" />
-    <button class="bg-blue p-2 border rounded-lg" type="submit">Connexion</button>
+  <form @submit.prevent="login" class="flex flex-col gap-4">
+    <input
+      class="p-2 rounded"
+      type="email"
+      name="UserEmail"
+      id="user_email"
+      v-model="email"
+      placeholder="Email"
+    />
+    <input
+      class="p-2 rounded"
+      type="password"
+      name="UserPassword"
+      id="user_password"
+      v-model="password"
+      placeholder="Mot de passe"
+    />
+    <p class="m-0 text-red text-center" v-if="incorrectAuth">Email ou mot de passe incorrect</p>
+    <button
+      class="p-2 border rounded-lg"
+      :class="{ 'bg-blue': isFormValid, 'bg-gray-400 cursor-not-allowed': !isFormValid }"
+      :disabled="!isFormValid || isLoading"
+      type="submit"
+    >
+      Connexion
+    </button>
   </form>
   <div class="flex gap-3 mt-2">
     <p class="m-0">Pas encore de compte ?</p>
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -10,6 +10,7 @@ const email = ref('')
 const password = ref('')
 
 const isLoading = ref(false)
+const alreadyRegistered = ref(false)
 
 const isFormValid = computed(() => {
   return !!username.value.trim() && !!teamName.value.trim() && !!email.value.trim() && !!password.value.trim()
@@ -33,8 +34,12 @@ function register () {
   .then((response) => {
     if (response.ok) {
       return response.json()
+    } else if (response.status === 400) {
+      alreadyRegistered.value = true
+      return Promise.reject('User already registered')
     } else {
-      throw new Error('error during registration')
+      console.log('Registration failed')
+      return Promise.reject('Registration failed')
     }
   })
   .then((data) => {
@@ -57,6 +62,7 @@ function register () {
     <input  class="p-2 rounded" type="text" name="TeamName" id="team_name" v-model="teamName" placeholder="Nom d'équipe">
     <input class="p-2 rounded" type="email" name="UserEmail" id="user_email" v-model="email" placeholder="Email" />
     <input class="p-2 rounded" type="password" name="UserPassword" id="user_password" v-model="password" placeholder="Mot de passe" />
+    <p class="m-0 text-red text-center" v-if="alreadyRegistered">Vous êtes déjà inscrit</p>
     <button
       class="p-2 border rounded-lg"
       :class="{'bg-blue': isFormValid, 'bg-gray-400 cursor-not-allowed': !isFormValid}"
@@ -73,6 +79,6 @@ function register () {
   </form>
   <div class="flex gap-3 mt-2">
     <p class="m-0">Déjà inscrit ?</p>
-    <router-link to="/">Me connecter</router-link>
+    <router-link to="/login">Me connecter</router-link>
   </div>
 </template>
