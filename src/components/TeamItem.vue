d7c7232229179b82e877f98fbe151b83141341fe
Alexis LOYANT <alexis.loyany@unicaen.fr>
2025-04-08 00:31:08 +0200

    Add leaderboard

---
+++ src/components/TeamItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import { defineProps } from 'vue'
+
+const props = defineProps(["team", "index", "score"])
+
+
+const teamName = ref(props.team)
+const teamScore = ref(props.score)
+const teamRank = ref(props.index + 1)
+
+</script>
+
+<template>
+  <div class="rounded-lg border b-white b-solid flex gap-2 px-5 m-2 w-200% justify-between">
+    <p>{{ teamRank }}</p>
+    <p>{{ teamName }}</p>
+    <p>{{ teamScore }}pts</p>
+  </div>
+</template>
--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -1,12 +1,44 @@
 <script setup>
+import { ref } from 'vue';
 import SideBar from '@/components/SideBar.vue';
+import TeamItem from '@/components/TeamItem.vue';
+import { onMounted } from 'vue';
+
+const teams = ref([])
+onMounted(() => {
+  fetch("http://localhost:3000/ranking")
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        console.log('Failed to fetch teams')
+        return Promise.reject('Failed to fetch teams')
+      }
+    })
+    .then((data) => {
+      teams.value = data
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+})
+
 </script>
 
 <template>
   <div class="flex gap-2">
     <SideBar/>
-    <div>
-      dkjdk
+    <div class="p-10">
+      <h2>Classement général</h2>
+      <div class="p-5">
+        <TeamItem
+          v-for="(team, index) in teams"
+          :key="index"
+          :team="team.team"
+          :index="index"
+          :score="team.points"
+          />
+      </div>
     </div>
   </div>
 </template>
